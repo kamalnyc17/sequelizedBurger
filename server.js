ffa0630eb8882bcdc3bde8ad01a212fc24a47eed
Kamal Mukherjee <kamalnyc17@gmail.com>
2019-04-21 16:53:23 -0400

    Devour the Burger with Sequelize

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var PORT = process.env.PORT || 8080;
 // launching the server
 var app = express();
 
+// Requiring our models for syncing
+var db = require("./models");
+
 // serve static content for the app from the 'public' folder
 app.use(express.static('public'));
 
@@ -19,11 +22,14 @@ app.use(express.json());
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
-app.use(routes);
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  console.log("Server listening on: http://localhost:" + PORT);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
